131e99d1002dac7eeeac057ceac94d898a812c9d
Draycia <lonelyyordle@gmail.com>
2020-07-21 00:10:13 -0700

    Remove unnecessary methods

--- src/main/java/de/themoep/minedown/Util.java
+++ src/main/java/de/themoep/minedown/Util.java
@@ -22,20 +22,11 @@ package de.themoep.minedown;
  * SOFTWARE.
  */
 
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
 
 import java.awt.Color;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Util {
@@ -55,75 +46,6 @@ public class Util {
     }
 
     /**
-     * Apply a collection of colors/formats to a component
-     * @param component The BaseComponent
-     * @param formats   The collection of ChatColor formats to apply
-     * @return The component that was modified
-     */
-    public static BaseComponent applyFormat(BaseComponent component, Collection<ChatColor> formats) {
-        for (ChatColor format : formats) {
-            if (format == ChatColor.BOLD) {
-                component.setBold(true);
-            } else if (format == ChatColor.ITALIC) {
-                component.setItalic(true);
-            } else if (format == ChatColor.UNDERLINE) {
-                component.setUnderlined(true);
-            } else if (format == ChatColor.STRIKETHROUGH) {
-                component.setStrikethrough(true);
-            } else if (format == ChatColor.MAGIC) {
-                component.setObfuscated(true);
-            } else if (format == ChatColor.RESET) {
-                component.setBold(false);
-                component.setItalic(false);
-                component.setUnderlined(false);
-                component.setStrikethrough(false);
-                component.setObfuscated(false);
-                component.setColor(ChatColor.WHITE);
-            } else {
-                component.setColor(format);
-            }
-        }
-        if (component.getExtra() != null) {
-            for (BaseComponent extra : component.getExtra()) {
-                applyFormat(extra, formats);
-            }
-        }
-        return component;
-    }
-
-    /**
-     * Apply a collection of colors/formats to a component builder
-     * @param builder The ComponentBuilder
-     * @param formats The collection of ChatColor formats to apply
-     * @return The component builder that was modified
-     */
-    public static ComponentBuilder applyFormat(ComponentBuilder builder, Collection<ChatColor> formats) {
-        for (ChatColor format : formats) {
-            if (format == ChatColor.BOLD) {
-                builder.bold(true);
-            } else if (format == ChatColor.ITALIC) {
-                builder.italic(true);
-            } else if (format == ChatColor.UNDERLINE) {
-                builder.underlined(true);
-            } else if (format == ChatColor.STRIKETHROUGH) {
-                builder.strikethrough(true);
-            } else if (format == ChatColor.MAGIC) {
-                builder.obfuscated(true);
-            } else if (format == ChatColor.RESET) {
-                builder.bold(false);
-                builder.italic(false);
-                builder.underlined(false);
-                builder.strikethrough(false);
-                builder.obfuscated(false);
-                builder.color(ChatColor.WHITE);
-            } else {
-                builder.color(format);
-            }
-        }
-        return builder;
-    }
-
-    /**
      * Check whether or not a character at a certain index of a string repeats itself
      * @param string The string to check
      * @param index  The index at which to check the character
@@ -131,41 +53,6 @@ public class Util {
      */
     public static boolean isDouble(String string, int index) {
         return index + 1 < string.length() && string.charAt(index) == string.charAt(index + 1);
-    }
-
-    /**
-     * Check whether a certain ChatColor is formatting or not
-     * @param format The ChatColor to check
-     * @return <tt>true</tt> if it's a format, <tt>false</tt> if it's a color
-     */
-    public static boolean isFormat(ChatColor format) {
-        return !MineDown.getFormatString(format).isEmpty();
-    }
-
-    /**
-     * Get a set of ChatColor formats all formats that a component includes
-     * @param component    The component to get the formats from
-     * @param ignoreParent Whether or not to include the parent's format
-     * @return A set of all the format ChatColors that the component includes
-     */
-    public static Set<ChatColor> getFormats(BaseComponent component, boolean ignoreParent) {
-        Set<ChatColor> formats = new LinkedHashSet<>();
-        if ((!ignoreParent && component.isBold()) || (component.isBoldRaw() != null && component.isBoldRaw())) {
-            formats.add(ChatColor.BOLD);
-        }
-        if ((!ignoreParent && component.isItalic()) || (component.isItalicRaw() != null && component.isItalicRaw())) {
-            formats.add(ChatColor.ITALIC);
-        }
-        if ((!ignoreParent && component.isUnderlined()) || (component.isUnderlinedRaw() != null && component.isUnderlinedRaw())) {
-            formats.add(ChatColor.UNDERLINE);
-        }
-        if ((!ignoreParent && component.isStrikethrough()) || (component.isStrikethroughRaw() != null && component.isStrikethroughRaw())) {
-            formats.add(ChatColor.STRIKETHROUGH);
-        }
-        if ((!ignoreParent && component.isObfuscated()) || (component.isObfuscatedRaw() != null && component.isObfuscatedRaw())) {
-            formats.add(ChatColor.MAGIC);
-        }
-        return formats;
     }
 
     /**
@@ -256,62 +143,6 @@ public class Util {
             lines.add(currentLine.toString());
         }
         return String.join("\n", lines);
-    }
-
-    private static Map<ChatColor, Color> legacyColors = new LinkedHashMap<>();
-
-    static {
-        legacyColors.put(ChatColor.BLACK, new Color(0x000000));
-        legacyColors.put(ChatColor.DARK_BLUE, new Color(0x0000AA));
-        legacyColors.put(ChatColor.DARK_GREEN, new Color(0x00AA00));
-        legacyColors.put(ChatColor.DARK_AQUA, new Color(0x00AAAA));
-        legacyColors.put(ChatColor.DARK_RED, new Color(0xAA0000));
-        legacyColors.put(ChatColor.DARK_PURPLE, new Color(0xAA00AA));
-        legacyColors.put(ChatColor.GOLD, new Color(0xFFAA00));
-        legacyColors.put(ChatColor.GRAY, new Color(0xAAAAAA));
-        legacyColors.put(ChatColor.DARK_GRAY, new Color(0x555555));
-        legacyColors.put(ChatColor.BLUE, new Color(0x05555FF));
-        legacyColors.put(ChatColor.GREEN, new Color(0x55FF55));
-        legacyColors.put(ChatColor.AQUA, new Color(0x55FFFF));
-        legacyColors.put(ChatColor.RED, new Color(0xFF5555));
-        legacyColors.put(ChatColor.LIGHT_PURPLE, new Color(0xFF55FF));
-        legacyColors.put(ChatColor.YELLOW, new Color(0xFFFF55));
-        legacyColors.put(ChatColor.WHITE, new Color(0xFFFFFF));
-    }
-
-    /**
-     * Utility method to remove RGB colors from components. This modifies the input array!
-     * @param components    The components to remove the rgb colors from
-     * @return The modified components (same as input).
-     */
-    public static BaseComponent[] rgbColorsToLegacy(BaseComponent[] components) {
-        for (BaseComponent component : components) {
-            if (component.getColorRaw() != null && component.getColorRaw().getName().startsWith("#")) {
-                component.setColor(getClosestLegacy(new Color(Integer.parseInt(component.getColorRaw().getName().substring(1), 16))));
-            }
-            if (component.getExtra() != null) {
-                rgbColorsToLegacy(component.getExtra().toArray(new BaseComponent[0]));
-            }
-        }
-        return components;
-    }
-
-    /**
-     * Get the legacy color closest to a certain RGB color
-     * @param color The color to get teh closest legacy color for
-     * @return The closest legacy color
-     */
-    public static ChatColor getClosestLegacy(Color color) {
-        ChatColor closest = null;
-        double smallestDistance = Double.MAX_VALUE;
-        for (Map.Entry<ChatColor, Color> legacy : legacyColors.entrySet()) {
-            double distance = distance(color, legacy.getValue());
-            if (distance < smallestDistance) {
-                smallestDistance = distance;
-                closest = legacy.getKey();
-            }
-        }
-        return closest;
     }
 
     /**
